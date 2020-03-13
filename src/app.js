949e133ded44e24d43465be1bcc0ccdb4cf8e7bb
ramyasashi <ramyakrishna676@gmail.com>
2020-03-13 13:07:59 +0530

    angular

--- src/app.js
+++ src/app.js
@@ -4,8 +4,8 @@ const port = process.env.PORT || 3000;
 const app = express();
 const angDir = path.join(__dirname,"./testProject");
 app.use(express.static(angDir));
-app.get('',(req,res) => {
-    res.send("HI,Welcome to heroku!")
+app.get('/*',(req,res) => {
+    res.sendFile(angDir);
 })
 app.listen(port,()=> {
     console.log(`Server runs:${port}`)
