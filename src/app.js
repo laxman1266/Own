60a19bf8fe030139e3a97aa95fbb406116be2350
ramyasashi <ramyakrishna676@gmail.com>
2020-03-13 13:06:31 +0530

    angular

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 const express = require("express");
+const path = require('path');
 const port = process.env.PORT || 3000;
 const app = express();
+const angDir = path.join(__dirname,"./testProject");
+app.use(express.static(angDir));
 app.get('',(req,res) => {
     res.send("HI,Welcome to heroku!")
 })
